bc60df91c5c1233262725809353a4f294c677c91
Anton Mazur <greenwood1dev@gmail.com>
2022-09-04 10:41:00 -0700

    git init & add file gitignore

---
+++ src/main/java/com/hillel/greenwoo/homeworks/homework2/Motherland.java
@@ -0,0 +1,14 @@
+package com.hillel.greenwoo.homeworks.homework2;
+
+public class Motherland {
+    public static void main(String[] args) {
+        double latitude;
+        double longitude;
+
+        latitude = 50.426569D;
+        longitude = 30.562971D;
+
+        System.out.println("Location of Motherland is " + latitude + " latitude and " +
+                longitude + " longitude");
+    }
+}
