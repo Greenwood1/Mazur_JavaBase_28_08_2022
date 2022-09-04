a170b0b14df47d2cb5aaf986f2d66fcab8a7f30b
Anton Mazur <greenwood1dev@gmail.com>
2022-09-04 11:41:19 -0700

    Add new file for Github

---
+++ src/main/java/com/hillel/greenwoo/homeworks/homework3/Git.java
@@ -0,0 +1,7 @@
+package com.hillel.greenwoo.homeworks.homework3;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Something new for GitHub");
+    }
+}
