0f5bedbf0972296809204b9d8c2b879996da6bd8
Anton Mazur <greenwood1dev@gmail.com>
2022-09-12 21:15:02 +0300

    homework CS

--- src/main/java/com/hillel/greenwoo/homeworks/homework5/CounterStrike.java
+++ src/main/java/com/hillel/greenwoo/homeworks/homework5/CounterStrike.java
@@ -4,13 +4,37 @@ package com.hillel.greenwoo.homeworks.homework5;
 import java.util.Scanner;
 
 public class CounterStrike {
+    public static final int COUNT_PLAYERS_IN_TEAM = 5;
 
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name for first team:");
+        String firstTeam = scanner.nextLine();
 
+        System.out.println("Enter name for second team:");
+        String secondTeam = scanner.nextLine();
 
-    public static String setNameTeam() {
-        Scanner scaner = new Scanner(System.in);
-        String nameTeam = scaner.nextLine();
-        return nameTeam;
+        System.out.println("Enter points for first team: ");
+        int pointFirstTeam = 0;
+        for (int i = 0; i< COUNT_PLAYERS_IN_TEAM; i++) {
+            int point = scanner.nextInt();
+            pointFirstTeam += point;
+        }
+
+        System.out.println("Enter points for second team: ");
+        int pointSecondTeam = 0;
+        for (int i = 0; i< COUNT_PLAYERS_IN_TEAM; i++) {
+            int point = scanner.nextInt();
+            pointSecondTeam += point;
+        }
+
+        if (pointFirstTeam > pointSecondTeam) {
+            System.out.println("win " + firstTeam + " with " + pointFirstTeam + " points");
+        } else if (pointFirstTeam < pointSecondTeam) {
+            System.out.println("win " + secondTeam + " with " + pointSecondTeam + " points");
+        } else {
+            System.out.println("draw");
+        }
+
     }
 }
